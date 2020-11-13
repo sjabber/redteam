21f3beafe545da9a91dcb2b2156fd838e79fb8b8
Taeho <sjabber91@gmail.com>
2020-11-13 17:53:21 +0900

    CreateUsers 수정

--- api/api_CreateUsers.go
+++ api/api_CreateUsers.go
@@ -21,20 +21,9 @@ func CreateUser(c *gin.Context) {
 	num, err = user.CreateUsers()
 	if err != nil { //user.CreateUser 에 대한 오류검사
 		log.Println(err.Error())
-		c.JSON(num, gin.H{"error": err.Error()})
+		c.JSON(num, gin.H{"error" : err.Error()})
 		return
 	}
 
-	accessToken, refreshToken, err := user.GetAuthToken()
-	if err == nil { // JWToken 에 대한 오류검사
-		c.JSON(http.StatusOK, gin.H{
-			"access-token": accessToken,
-			"refresh-token": refreshToken,
-		})
-		return
-	} else {
-		c.JSON(500, gin.H{"Create error occurred, account : ": user.Email})
-		return
-	}
-
+	c.JSON(http.StatusOK, gin.H{"You are now a member, account ": user.Email})
 }
