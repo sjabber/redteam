1ef6bf430b293e0153b8ccb6b068309fe31516bc
Taeho <sjabber91@gmail.com>
2020-12-08 19:27:03 +0900

    getTag api 완료

--- api/api_CreateProject.go
+++ api/api_CreateProject.go
@@ -11,8 +11,8 @@ func GetTag(c *gin.Context) {
 	num := c.Keys["number"].(int)
 
 	err := model.GetTag2(num)
-	if err != nil {
-		log.Println("GetTag error occurred during CreateProject, account :", c.Keys["email"])
+	if err == nil {
+		log.Println("GetTag error occurred, account :", c.Keys["email"])
 	}
 
 	c.JSON(http.StatusOK, gin.H{"tags" : err})
