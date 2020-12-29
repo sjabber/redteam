3379f3f0a2efb2bede03b436580805fbf421e72f
Taeho <sjabber91@gmail.com>
2020-12-30 02:42:05 +0900

    #65 DB connection 환경변수

--- model/db.go
+++ model/db.go
@@ -3,11 +3,24 @@ package model
 import (
 	"database/sql"
 	_ "github.com/lib/pq"
+	"os"
+)
+
+var (
+	dbUser = os.Getenv("DB_USER")
+	dbPw = os.Getenv("DB_PW")
+	dbName = os.Getenv("DB_NAME")
+	dbHost = os.Getenv("DB_HOST")
 )
 
 func ConnectDB() (*sql.DB, error) {
+	//Note local db 연결시 사용하는 코드
+	//db, err := sql.Open("postgres", "user=postgres password=1673" +
+	//	" dbname=postgres host=localhost sslmode=disable port=5432")
+
 	db, err := sql.Open("postgres",
-		"user=postgres password=1673 dbname=postgres host=localhost sslmode=disable port=5432")
+		"user=" + dbUser+ " password="+ dbPw+ " dbname="+ dbName +
+		" host="+ dbHost +" sslmode=disable port=5432")
 	if db != nil {
 		db.SetMaxOpenConns(100)
 		db.SetMaxIdleConns(10)
