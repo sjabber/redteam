8b45e02aa2235d22275e088f84d36adc4ee6c2d2
bourbonkk <pig3653@gmail.com>
2021-04-04 20:08:00 +0900

    #113 | 개발 로깅제거

--- ui/html/server.js
+++ ui/html/server.js
@@ -113,6 +113,6 @@ app.use(function (req, res, next) {
 });
 
 
-server.listen(8888, function () {
+server.listen(8080, function () {
     console.log('Express server listening on port ' + server.address().port);
 })
